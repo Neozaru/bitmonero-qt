0c958c11bfe1b9fdefeb8d42b7c5517baf88db3c
Neozaru <neozaru@mailoo.org>
2014-05-22 22:25:13 +0200

    Revert "Initial project structure"
    This reverts commit 68ceb2c524576c310493c346c7887f9a69fafadc.

--- src/Models/MoneroModel.cpp
+++
@@ -1,5 +0,0 @@
-#include "MoneroModel.h"
-
-MoneroModel::MoneroModel()
-{
-}
--- src/RPC/RPCWallet.cpp
+++
@@ -1,9 +0,0 @@
-#include "RPCWallet.h"
-
-RPCWallet::RPCWallet(WalletModel& pModel, const QString& pHost, unsigned int pPort)
-    : WalletInterface(pModel), rpc(pHost,pPort)
-{
-
-    pModel.setWalletInterface(this);
-
-}
