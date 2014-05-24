e31334f5a8f744e4f2d14261ff29c3bbea916afc
Neozaru <neozaru@mailoo.org>
2014-05-24 18:07:22 +0200

    Added config file instructions

--- app/main.cpp
+++ app/main.cpp
@@ -8,6 +8,10 @@
 #include <QQmlContext>
 #include <QQmlComponent>
 
+#include <QSettings>
+#include <QStandardPaths>
+#include <QDir>
+
 #include "Models/MoneroModel.h"
 #include "Models/WalletModel.h"
 #include "Models/MinerModel.h"
@@ -20,6 +24,31 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    /* Loads settings. TODO : Put an interface in the GUI */
+    const QString& lConfigFile = QDir::homePath() + "/.bitmonero-qt/bitmonero-qt.conf";
+    QSettings lSettings(lConfigFile, QSettings::IniFormat);
+
+    lSettings.beginGroup("MainConfig");
+
+    const QString& lMoneroUri = lSettings.value("daemon_uri", "localhost/json_rpc").toString();
+    int lMoneroPort = lSettings.value("daemon_port", 18081).toInt();
+
+    const QString& lWalletUri = lSettings.value("wallet_uri", "localhost").toString();
+    int lWalletPort = lSettings.value("wallet_port", 19091).toInt();
+
+    const QString& lMinerUri = lSettings.value("miner_uri", "localhost").toString();
+    int lMinerPort = lSettings.value("miner_port", 19091).toInt();
+
+    lSettings.endGroup();
+
+
+    qDebug() << "[Loaded config]";
+    qDebug() << "Daemon : " << lMoneroUri << " " << lMoneroPort;
+    qDebug() << "Wallet : " << lWalletUri << " " << lWalletPort;
+    qDebug() << "Miner : " << lMinerUri << " " << lMinerPort;
+    /* */
+
 
     qmlRegisterType<WalletModel>("info.neozaru.bitmonero-qt.walletmodel", 1, 0, "WalletModel");
 
