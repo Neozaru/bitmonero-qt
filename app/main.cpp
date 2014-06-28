33563feca4f75a85dfeeb8683a3f4925e7968a3c
Neozaru <neozaru@mailoo.org>
2014-06-28 12:55:32 +0200

    Attempting to add icon on windows

--- app/main.cpp
+++ app/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-    QIcon lIcon(":/qml/res/MoneroWhite.ico");
+    QIcon lIcon(":qml/res/MoneroWhite.ico");
     app.setWindowIcon(lIcon);
 
     MoneroGUI lGui(app);
